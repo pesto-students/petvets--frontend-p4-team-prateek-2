a7bad6cba89a0cd27328f82e00bfe97579d05fb0
abhishekraje30 <abhishekraje30@gmail.com>
2023-01-22 08:58:37 +0530

    notification added in clinic details

--- src/components/profile/ClinicDetails.js
+++ src/components/profile/ClinicDetails.js
@@ -1,7 +1,14 @@
-import { Autocomplete, Button, Grid, TextField } from '@mui/material';
+import {
+  Alert,
+  Autocomplete,
+  Button,
+  Grid,
+  Snackbar,
+  TextField,
+} from '@mui/material';
 import { Box } from '@mui/system';
 import { useFormik } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation } from 'react-query';
 import { useDispatch, useSelector } from 'react-redux';
 import * as Yup from 'yup';
@@ -34,6 +41,8 @@ const profileValidation = Yup.object({
 
 const ClinicDetails = () => {
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
   const { userId, userData } = useSelector((state) => state.authStatus);
   const { role } = userData;
 
@@ -76,193 +85,215 @@ const ClinicDetails = () => {
         userId,
         ...userDetails,
       });
+
+      setSnackbarMessage('User updated successfully');
+      setOpen(true);
     },
   });
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
   return (
-    <form onSubmit={formik.handleSubmit} method="post">
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            autoComplete="given-name"
-            required
-            fullWidth
-            id="clinicName"
-            name="clinicName"
-            label="Clinic Name"
-            disabled={role === 'admin'}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.clinicName}
-            error={
-              formik.touched.clinicName && Boolean(formik.errors.clinicName)
-            }
-            helperText={formik.touched.clinicName && formik.errors.clinicName}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            autoComplete="given-name"
-            required
-            fullWidth
-            id="clinicAddress1"
-            disabled={role === 'admin'}
-            name="clinicAddress1"
-            label="Street/Colony Name"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.clinicAddress1}
-            error={
-              formik.touched.clinicAddress1 &&
-              Boolean(formik.errors.clinicAddress1)
-            }
-            helperText={
-              formik.touched.clinicAddress1 && formik.errors.clinicAddress1
-            }
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            autoComplete="given-name"
-            required
-            fullWidth
-            id="clinicAddress2"
-            disabled={role === 'admin'}
-            name="clinicAddress2"
-            label="Area Name"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.clinicAddress2}
-            error={
-              formik.touched.clinicAddress2 &&
-              Boolean(formik.errors.clinicAddress2)
-            }
-            helperText={
-              formik.touched.clinicAddress2 && formik.errors.clinicAddress2
-            }
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            autoComplete="given-name"
-            required
-            fullWidth
-            id="clinicCity"
-            name="clinicCity"
-            label="City"
-            disabled={role === 'admin'}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.clinicCity}
-            error={
-              formik.touched.clinicCity && Boolean(formik.errors.clinicCity)
-            }
-            helperText={formik.touched.clinicCity && formik.errors.clinicCity}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Autocomplete
-            disablePortal
-            id="combo-box-demo"
-            options={states}
-            disabled={role === 'admin'}
-            isOptionEqualToValue={(option, value) => option.label === value}
-            value={formik.values.clinicState}
-            onChange={(_, value) =>
-              formik.setFieldValue('clinicState', value?.label)
-            }
-            renderInput={(params) => (
-              <TextField
-                onBlur={formik.handleBlur}
-                fullWidth
-                error={
-                  formik.touched.clinicState &&
-                  Boolean(formik.errors.clinicState)
-                }
-                helperText={
-                  formik.touched.clinicState && formik.errors.clinicState
-                }
-                {...params}
-                label="State"
-              />
-            )}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            autoComplete="given-name"
-            required
-            fullWidth
-            id="clinicPincode"
-            name="clinicPincode"
-            label="Pincode"
-            disabled={role === 'admin'}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.clinicPincode}
-            error={
-              formik.touched.clinicPincode &&
-              Boolean(formik.errors.clinicPincode)
-            }
-            helperText={
-              formik.touched.clinicPincode && formik.errors.clinicPincode
-            }
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Autocomplete
-            disablePortal
-            id="combo-box-demo"
-            options={weeks}
-            isOptionEqualToValue={(option, value) =>
-              option.value === value.value
-            }
-            multiple={true}
-            disabled={role === 'admin'}
-            value={formik.values.clinicDaysOff}
-            onChange={(_, value) =>
-              formik.setFieldValue('clinicDaysOff', value)
-            }
-            getOptionLabel={(options) => options.label}
-            renderInput={(params) => (
-              <TextField
-                onBlur={formik.handleBlur}
-                fullWidth
-                error={
-                  formik.touched.clinicDaysOff &&
-                  Boolean(formik.errors.clinicDaysOff)
-                }
-                helperText={
-                  formik.touched.clinicDaysOff && formik.errors.clinicDaysOff
-                }
-                {...params}
-                label="Days Off"
-              />
-            )}
-          />
-        </Grid>
-      </Grid>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          pt: 2,
-        }}
+    <>
+      <Snackbar
+        open={open}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        autoHideDuration={6000}
+        onClose={handleClose}
       >
-        <Grid>
-          <Button color="inherit" onClick={() => dispatch(prevStepper())}>
-            Back
-          </Button>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+      <form onSubmit={formik.handleSubmit} method="post">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="given-name"
+              required
+              fullWidth
+              id="clinicName"
+              name="clinicName"
+              label="Clinic Name"
+              disabled={role === 'admin'}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.clinicName}
+              error={
+                formik.touched.clinicName && Boolean(formik.errors.clinicName)
+              }
+              helperText={formik.touched.clinicName && formik.errors.clinicName}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="given-name"
+              required
+              fullWidth
+              id="clinicAddress1"
+              disabled={role === 'admin'}
+              name="clinicAddress1"
+              label="Street/Colony Name"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.clinicAddress1}
+              error={
+                formik.touched.clinicAddress1 &&
+                Boolean(formik.errors.clinicAddress1)
+              }
+              helperText={
+                formik.touched.clinicAddress1 && formik.errors.clinicAddress1
+              }
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="given-name"
+              required
+              fullWidth
+              id="clinicAddress2"
+              disabled={role === 'admin'}
+              name="clinicAddress2"
+              label="Area Name"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.clinicAddress2}
+              error={
+                formik.touched.clinicAddress2 &&
+                Boolean(formik.errors.clinicAddress2)
+              }
+              helperText={
+                formik.touched.clinicAddress2 && formik.errors.clinicAddress2
+              }
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="given-name"
+              required
+              fullWidth
+              id="clinicCity"
+              name="clinicCity"
+              label="City"
+              disabled={role === 'admin'}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.clinicCity}
+              error={
+                formik.touched.clinicCity && Boolean(formik.errors.clinicCity)
+              }
+              helperText={formik.touched.clinicCity && formik.errors.clinicCity}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={states}
+              disabled={role === 'admin'}
+              isOptionEqualToValue={(option, value) => option.label === value}
+              value={formik.values.clinicState}
+              onChange={(_, value) =>
+                formik.setFieldValue('clinicState', value?.label)
+              }
+              renderInput={(params) => (
+                <TextField
+                  onBlur={formik.handleBlur}
+                  fullWidth
+                  error={
+                    formik.touched.clinicState &&
+                    Boolean(formik.errors.clinicState)
+                  }
+                  helperText={
+                    formik.touched.clinicState && formik.errors.clinicState
+                  }
+                  {...params}
+                  label="State"
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="given-name"
+              required
+              fullWidth
+              id="clinicPincode"
+              name="clinicPincode"
+              label="Pincode"
+              disabled={role === 'admin'}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.clinicPincode}
+              error={
+                formik.touched.clinicPincode &&
+                Boolean(formik.errors.clinicPincode)
+              }
+              helperText={
+                formik.touched.clinicPincode && formik.errors.clinicPincode
+              }
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={weeks}
+              isOptionEqualToValue={(option, value) =>
+                option.value === value.value
+              }
+              multiple={true}
+              disabled={role === 'admin'}
+              value={formik.values.clinicDaysOff}
+              onChange={(_, value) =>
+                formik.setFieldValue('clinicDaysOff', value)
+              }
+              getOptionLabel={(options) => options.label}
+              renderInput={(params) => (
+                <TextField
+                  onBlur={formik.handleBlur}
+                  fullWidth
+                  error={
+                    formik.touched.clinicDaysOff &&
+                    Boolean(formik.errors.clinicDaysOff)
+                  }
+                  helperText={
+                    formik.touched.clinicDaysOff && formik.errors.clinicDaysOff
+                  }
+                  {...params}
+                  label="Days Off"
+                />
+              )}
+            />
+          </Grid>
         </Grid>
-        <Grid>
-          {role !== 'admin' ? (
-            <Button type="submit" variant="contained">
-              Save
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            pt: 2,
+          }}
+        >
+          <Grid>
+            <Button color="inherit" onClick={() => dispatch(prevStepper())}>
+              Back
             </Button>
-          ) : null}
-        </Grid>
-      </Box>
-    </form>
+          </Grid>
+          <Grid>
+            {role !== 'admin' ? (
+              <Button type="submit" variant="contained">
+                Save
+              </Button>
+            ) : null}
+          </Grid>
+        </Box>
+      </form>
+    </>
   );
 };
 
